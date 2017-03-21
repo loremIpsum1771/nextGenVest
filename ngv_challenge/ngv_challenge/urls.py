9ac40d5e79b4d4bcb2a66b8d673d058cf67f87ad
Ceyron <ceh451@nyu.edu>
2017-03-21 03:03:56 -0400

    Django REST added

---
+++ ngv_challenge/ngv_challenge/urls.py
@@ -0,0 +1,9 @@
+"""ngv_challenge URL Configuration
+
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
---
+++ ngv_challenge/scholarship_list/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ScholarshipListConfig(AppConfig):
+    name = 'scholarship_list'
